5b7511225c5931a7d45ca8ff3142c78ef746d215
ratu <ratukf@gmail.com>
2026-02-03 16:59:33 +0700

    feat: add download cv button

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,25 @@ export default function Home() {
       <Projects />
       <Contact />
       <Footer />
+      <a
+        href="/CV_RatuKilaFatimah.pdf"
+        download
+        className="
+          fixed z-50
+          right-4 bottom-4
+          md:bottom-auto md:top-1/2 md:-translate-y-1/2 md:right-6
+
+          bg-black text-white
+          p-4 rounded-full
+          border border-white
+          shadow-lg
+
+          transition hover:scale-105
+        "
+        aria-label="Download CV"
+      >
+        Download my CV ⬇️
+      </a>
     </main>
   );
 }
